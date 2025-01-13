d69aaf49554420bc72f069430d5485f49352b11a
patimakerr <varsan.mv@bk.ru>
2025-01-13 18:15:23 +0300

    исправление ошибок

--- homework03/maze.py
+++ homework03/maze.py
@@ -1,7 +1,10 @@
 from copy import deepcopy
 from random import choice, randint
 from typing import List, Optional, Tuple, Union
+
 import pandas as pd
+
+
 def create_grid(rows: int = 15, cols: int = 15) -> List[List[Union[str, int]]]:
     return [["■"] * cols for _ in range(rows)]
 
